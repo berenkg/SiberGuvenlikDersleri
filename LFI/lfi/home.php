b9d02c7b070efc94bd7163079f89689bfb3bfd7c
root <root@localhost.localdomain>
2019-11-18 13:49:16 -0500

    LFI konusu eklendi

---
+++ LFI/lfi/home.php
@@ -0,0 +1,7 @@
+<div class="jumbotron">
+  <h1 class="display-4">Anasayfaya hoşgeldin!</h1>
+  <p class="lead">Loremler ipsumlar. Loremler ipsumlar. Loremler ipsumlar. Loremler ipsumlar. Loremler ipsumlar. Loremler ipsumlar. Loremler ipsumlar. </p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Devamını oku...</a>
+</div>
